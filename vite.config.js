be0d0930b56cabcf4111ad832d5a4cd3d9705784
Jaco Swarts <jaco@productive.me>
2025-01-01 12:51:12 +0200

    Make use of dotenv in vite

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,33 @@
-import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   server: {
     port: parseInt(process.env.VITE_PORT || 3000),
   },
-  root: "./client",
+  root: './client',
   plugins: [
     preact(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       manifest: {
-        name: "Modern PWA",
-        short_name: "PWA",
-        theme_color: "#ffffff",
+        name: 'yzerfontein-app',
+        short_name: 'yzerfontein-app',
+        theme_color: 'red',
         icons: [
           {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
@@ -32,9 +35,9 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/api\./,
-            handler: "NetworkFirst",
+            handler: 'NetworkFirst',
             options: {
-              cacheName: "api-cache",
+              cacheName: 'api-cache',
               networkTimeoutSeconds: 5,
               expiration: {
                 maxEntries: 50,
@@ -49,5 +52,5 @@ export default defineConfig({
       },
     }),
   ],
-  envDir: '../'
-});
+  envDir: '../',
+})
